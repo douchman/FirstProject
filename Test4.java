015a3cdd8d60e9507ca991d9a9907e6dbd23627c
sanghyun.Park <take6948@google.com>
2020-05-06 13:15:29 +0900

    Test4 delete

--- Test4.java
+++
@@ -1,8 +0,0 @@
-class Test4{
-	public static void main(String [] args){
-		System.out.println("i want go home right now1");
-		System.out.println("i want go home right now2");
-		System.out.println("i want go home right now3");
-		System.out.println("i want go home right now4");
-	}
-}
