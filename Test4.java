ba334fa58aba6204f6080cf8d0a789d08da4e417
sanghyun.Park <take6948@google.com>
2020-05-06 12:46:13 +0900

    Test4.java 추가

---
+++ Test4.java
@@ -0,0 +1,8 @@
+class Test4{
+	public static void main(String [] args){
+		System.out.println("i want go home right now1");
+		System.out.println("i want go home right now2");
+		System.out.println("i want go home right now3");
+		System.out.println("i want go home right now4");
+	}
+}
