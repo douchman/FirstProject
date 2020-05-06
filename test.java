babfbb5a778ea44bb568481ea4b6b1469ae856ff
sanghyun.Park <take6948@google.com>
2020-05-06 15:08:32 +0900

    test.java 주석 추가 및 한줄 추가!

--- test.java
+++ test.java
@@ -1,6 +1,7 @@
 class test{
 	public static void main(String [] args)
 //		System.out.println("hello");
-		System.out.println("hello2!!");
+//		System.out.println("hello2!!");
 		System.out.println("hello2!12131!");
+	 	System.out.println("hello2!12131!");
 }
